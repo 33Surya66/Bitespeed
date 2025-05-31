c30ec0e40eeb27879b430280c7a4d2012fc6c661
Surya Ravi <112548832+33Surya66@users.noreply.github.com>
2025-05-31 06:26:46 +0530

    Add Jest configuration for unit testing

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'js'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.{ts,js}']
+};
+
+export default config;
